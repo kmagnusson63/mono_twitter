eaffcaa06754ca5df828cb1b3c9f74b3a9161121
Ken Magnusson <kmagnusson63@academic.rrc.ca>
2013-11-04 13:22:42 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonoTwitter::Application.config.secret_token = 'ec5f90b752cc95aae89b6117c6a5cef3f8950d4a5b7e2eed7a55e94130804e01afb158a826f1c1958cbea873732e5cb89a5511de15d2ba9e49a7fc8d30d3c27f'
